a688902f5ec9577e4b696172af05aa33fb4c98bf
kazimalimq <ali.kazim@students.mq.edu.au>
2022-12-25 16:18:33 +1100

    Front end complete backend is working with get and post requests

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ class Poll{
         this.root.insertAdjacentHTML('afterbegin',`
         <div class = 'poll__title'>${title}<div/>
         
-        `)
+        `);
 
         this._refresh()
 
@@ -14,7 +14,43 @@ class Poll{
     async _refresh(){
         const response = await fetch(this.endpoint)
         const data = await response.json()
-        console.log(data)
+        this.root.querySelectorAll('.poll__option').forEach(option => {
+            option.remove();
+        });
+        for (const option of data) {
+            const template  = document.createElement('template');
+            const fragment = template.content
+
+            template.innerHTML = `
+            <div class="poll__option ${this.selected == option.lable? 'poll__option--selected': ''}">
+                <div class="poll__option-fill"> </div>
+                <div class="poll__option-info">
+                    <span class="poll__lable">${option.lable}</span>
+                    <span class="poll__percentage">${option.percentage}%</span>
+                </div>
+            </div>
+            `;
+
+        if (!this.selected){
+            fragment.querySelector('.poll__option').addEventListener('click', ()=> {
+                fetch(this.endpoint, {
+                    method: 'post',
+                    body: `add=${option.lable}`,
+                    headers :{
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }).then(()=>{
+                    this.selected = option.lable;
+
+                    sessionStorage.setItem('poll-selected', option.lable)
+                    this._refresh();
+                })
+            })
+        }
+
+        fragment.querySelector('.poll__option-fill').style.width = `${option.percentage}%`
+        this.root.appendChild(fragment);
+        }
     }
 }
 
