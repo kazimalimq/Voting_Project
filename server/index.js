ffc78df4b97dbfb01cb495e870b50546f16e0f7a
kazimalimq <ali.kazim@students.mq.edu.au>
2022-12-25 14:27:25 +1100

    1st end point added

--- server/index.js
+++ server/index.js
@@ -7,4 +7,22 @@ const app = express();
 
 const dataFile = path.join(__dirname, "data.json");
 
+//Support posting porm data with url encoded
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/poll', async(req, res)=>{
+
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"))
+    const totalVotes = Object.values(data).reduce((total, n)=> total += n , 0 )
+
+    data = Object.entries(data).map(([lable, votes]) => {
+        return {
+            lable,
+            percentage: (((votes *100)/totalVotes) || 0).toFixed(0)
+        }
+    })
+    res.json(data)
+});
+
 app.listen(3000, ()=> console.log("Server is running"));
